f7f2d10b87c5283f68794e8568f0e13c4b2e2f6d
Jiele Ye <yejiele1983@hotmail.com>
2023-03-23 15:29:53 +0800

    update uproject

--- Source/SampleNHSDKUE427/SampleNHSDKUE427.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SampleNHSDKUE427.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SampleNHSDKUE427, "SampleNHSDKUE427" );
--- Source/SampleNHSDKUE427/SampleNHSDKUE427GameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "SampleNHSDKUE427GameModeBase.h"
-
