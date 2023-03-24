e7f8d12202ed00f03379aafd184c85278eb7882b
Jiele Ye <yejiele1983@hotmail.com>
2023-03-24 11:38:32 +0800

    update Startup.bat

--- Startup.bat
+++ Startup.bat
@@ -8,10 +8,10 @@ rem Fetch NextCAS-SDK, SDK Plugin
 git clone git@118.31.50.10:tudou/NextCAS-SDK.git %DEMO_ROOT%\Plugins\NextCAS-SDK -b main
 
 rem Fetch NHMiniZip, Zip Plugin
-git clone git@118.31.50.10:tudou/NHMiniZip.git %DEMO_ROOT%\Plugins\NHMiniZip -b main
+rem git clone git@118.31.50.10:tudou/NHMiniZip.git %DEMO_ROOT%\Plugins\NHMiniZip -b main
 
 rem Fetch NHMMKV, Key-Value cache Plugin
-git clone git@118.31.50.10:tudou/NHMMKV.git %DEMO_ROOT%\Plugins\NHMMKV -b main
+rem git clone git@118.31.50.10:tudou/NHMMKV.git %DEMO_ROOT%\Plugins\NHMMKV -b main
 
 rem Fetch NextCAS-Assets
 rem git clone git@118.31.50.10:tudou/NextCAS-Assets.git %DEMO_ROOT%\Plugins\NextCAS-Assets -b main
