ddf58d328a9fe2d4a2abcddab1acc0ea29615eb5
Jiele Ye <yejiele1983@hotmail.com>
2023-04-14 14:46:09 +0800

    add ANextAvatar.RemoveBundle

--- Source/SampleNHSDKUE427/Private/AvatarLoader.cpp
+++ Source/SampleNHSDKUE427/Private/AvatarLoader.cpp
@@ -50,16 +50,47 @@ void AAvatarLoader::LoadAvatar() {
     if (!Avatar) {
         return;
     }
+    //female
     static const FString AvatarId = TEXT("avatar_64241b207f42de7d0775140d");
     static const FString BodyId = TEXT("body_642413f6178b9538264e6a8e");
     static const FString CoverallId = TEXT("coverall_64312f6f10809e0df45537a7");
-
+    static const FString ClothId = TEXT("cloth_6437c90d9181074839341076");
+    static const FString ShoesId = TEXT("shoes_6437d1b39181074839341077");
+    static const FString TrouserId = TEXT("trouser_6437d2829181074839341078");
+    
     INextHumanSDKModule::Get().PrepareResource(BodyId, [=](int32 Code, const FString& Message) {
-        Avatar->SetAvatarId(/* Put Avatar Id here */ AvatarId, [=](int32 Code, const FString& Message) {
+        Avatar->SetAvatarId(/* Put Avatar Id here */ AvatarId, [=](int32 Code, const FString& Message, int64 Id) {
             UE_LOG(LogTemp, Warning, TEXT("SetAvatarId(%s): %d %s"), *AvatarId, Code, *Message);
             if (Code == 0) {
-                Avatar->AddBundleById(CoverallId, [=](int32 Code, const FString& Message) {
-                    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s"), *CoverallId, Code, *Message);
+                //Avatar->AddBundleById(CoverallId, [=](int32 Code, const FString& Message, int64 Id) {
+                //    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s %lld"), *CoverallId, Code, *Message, Id);
+                //});
+
+                Avatar->AddBundleById(ClothId, [=](int32 Code, const FString& Message, int64 Id) {
+                    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s %d"), *ClothId, Code, *Message, Id);
+                    FTimerHandle TH;
+                    GetWorldTimerManager().SetTimer(TH, [=]() {
+                        UE_LOG(LogTemp, Warning, TEXT("Try to remove %d"), Id);
+                        Avatar->RemoveBundle(Id);
+                        }, 5.0, false);
+                });
+
+                Avatar->AddBundleById(ShoesId, [=](int32 Code, const FString& Message, int64 Id) {
+                    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s %d"), *ShoesId, Code, *Message, Id);
+                    FTimerHandle TH;
+                    GetWorldTimerManager().SetTimer(TH, [=]() {
+                        UE_LOG(LogTemp, Warning, TEXT("Try to remove %d"), Id);
+                        Avatar->RemoveBundle(Id);
+                        }, 10.0, false);
+                });
+
+                Avatar->AddBundleById(TrouserId, [=](int32 Code, const FString& Message, int64 Id) {
+                    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s %d"), *TrouserId, Code, *Message, Id);
+                    FTimerHandle TH;
+                    GetWorldTimerManager().SetTimer(TH, [=]() {
+                        UE_LOG(LogTemp, Warning, TEXT("Try to remove %d"), Id);
+                        Avatar->RemoveBundle(Id);
+                        }, 15.0, false);
                 });
             }
         });
