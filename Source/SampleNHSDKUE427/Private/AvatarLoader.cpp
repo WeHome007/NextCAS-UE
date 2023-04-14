80cf35b1bfc71681b00dd71e9435b2d3f77d1fa7
Jiele Ye <yejiele1983@hotmail.com>
2023-04-14 18:56:51 +0800

    use RunChain to sync actions

--- Source/SampleNHSDKUE427/Private/AvatarLoader.cpp
+++ Source/SampleNHSDKUE427/Private/AvatarLoader.cpp
@@ -4,95 +4,136 @@
 #include "AvatarLoader.h"
 #include "INextHumanSDK.h"
 #include "NextAvatar.h"
+#include "RunChain.h"
 
 // Sets default values
 AAvatarLoader::AAvatarLoader()
 {
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
 }
 
 // Called when the game starts or when spawned
 void AAvatarLoader::BeginPlay()
 {
-    Super::BeginPlay();
-    static const FString TempAccessToken = "J+W+sX+b1zYiKgQcVLh5rlRCGbu8rKWrA/UYqfe6qSFIblSL0z0JQ8mF4Y6lYgkYuap95BSjcPhOOuLLr9cOw6TY/65sB7DfBBYCnoB8noxDXnj7s918/0nw+yvcY8WXl2HcbswMBPDiBGT/Gc6eQg==";
-    if (!INextHumanSDKModule::Get().IsInitialized()) {
+	Super::BeginPlay();
+	static const FString TempAccessToken = "J+W+sX+b1zYiKgQcVLh5rlRCGbu8rKWrA/UYqfe6qSFIblSL0z0JQ8mF4Y6lYgkYuap95BSjcPhOOuLLr9cOw6TY/65sB7DfBBYCnoB8noxDXnj7s918/0nw+yvcY8WXl2HcbswMBPDiBGT/Gc6eQg==";
+	if (!INextHumanSDKModule::Get().IsInitialized()) {
 
-        INextHumanSDKModule::Get().Initialize(/* Put your access token here */ TempAccessToken, /* optional: custom server address ,*/ [=](int32 Code, const FString& Message)
-        {
-            if (Code == INextHumanSDKModule::CODE_SUCCESS) {
-                LoadAvatar();
-            }
-        });
-    }
-    else {
-        LoadAvatar();
-    }
+		INextHumanSDKModule::Get().Initialize(/* Put your access token here */ TempAccessToken, /* optional: custom server address ,*/ [=](int32 Code, const FString& Message)
+		{
+			if (Code == INextHumanSDKModule::CODE_SUCCESS) {
+				LoadAvatar();
+			}
+		});
+	}
+	else {
+		LoadAvatar();
+	}
 }
 
 // Called every frame
 void AAvatarLoader::Tick(float DeltaTime)
 {
-    Super::Tick(DeltaTime);
+	Super::Tick(DeltaTime);
 
 }
 
 
 void AAvatarLoader::LoadAvatar() {
-    UWorld* World = GetWorld();
-    if (!World) {
-        return;
-    }
+	UWorld* World = GetWorld();
+	if (!World) {
+		return;
+	}
 
-    auto Avatar = World->SpawnActor<ANextAvatar>(FVector(0, 0, 20), FRotator(0, 0, 0));
-    if (!Avatar) {
-        return;
-    }
-    //female
-    static const FString AvatarId = TEXT("avatar_64241b207f42de7d0775140d");
-    static const FString BodyId = TEXT("body_642413f6178b9538264e6a8e");
-    static const FString CoverallId = TEXT("coverall_64312f6f10809e0df45537a7");
-    static const FString ClothId = TEXT("cloth_6437c90d9181074839341076");
-    static const FString ShoesId = TEXT("shoes_6437d1b39181074839341077");
-    static const FString TrouserId = TEXT("trouser_6437d2829181074839341078");
-    
-    INextHumanSDKModule::Get().PrepareResource(BodyId, [=](int32 Code, const FString& Message) {
-        Avatar->SetAvatarId(/* Put Avatar Id here */ AvatarId, [=](int32 Code, const FString& Message, int64 Id) {
-            UE_LOG(LogTemp, Warning, TEXT("SetAvatarId(%s): %d %s"), *AvatarId, Code, *Message);
-            if (Code == 0) {
-                //Avatar->AddBundleById(CoverallId, [=](int32 Code, const FString& Message, int64 Id) {
-                //    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s %lld"), *CoverallId, Code, *Message, Id);
-                //});
+	auto Avatar = World->SpawnActor<ANextAvatar>(FVector(0, 0, 20), FRotator(0, 0, 0));
+	if (!Avatar) {
+		return;
+	}
+	//female
+	static const FString AvatarId = TEXT("avatar_64241b207f42de7d0775140d");
+	static const FString BodyId = TEXT("body_642413f6178b9538264e6a8e");
+	static const FString CoverallId = TEXT("coverall_64312f6f10809e0df45537a7");
+	static const FString ClothId = TEXT("cloth_6437c90d9181074839341076");
+	static const FString ShoesId = TEXT("shoes_6437d1b39181074839341077");
+	static const FString TrousersId = TEXT("trouser_6437d2829181074839341078");
 
-                Avatar->AddBundleById(ClothId, [=](int32 Code, const FString& Message, int64 Id) {
-                    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s %d"), *ClothId, Code, *Message, Id);
-                    FTimerHandle TH;
-                    GetWorldTimerManager().SetTimer(TH, [=]() {
-                        UE_LOG(LogTemp, Warning, TEXT("Try to remove %d"), Id);
-                        Avatar->RemoveBundle(Id);
-                        }, 5.0, false);
-                });
+	using nexthuman::sdk::demo::FRet;
+	using nexthuman::sdk::demo::TTaskChain;
+	struct FTestRet : public FRet {
+		int64 ClothId;
+		int64 ShoesId;
+		int64 TrousersId;
+		FTestRet(int32 InCode, FString InMessage, int64 InClothId = INT64_MIN, int64 InShoesId = INT64_MIN, int64 InTrousersId = INT64_MIN):FRet(InCode, InMessage), ClothId(InClothId), ShoesId(InShoesId), TrousersId(InTrousersId) {}
+	};
+	typedef TTaskChain<FTestRet> FTaskChain;
 
-                Avatar->AddBundleById(ShoesId, [=](int32 Code, const FString& Message, int64 Id) {
-                    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s %d"), *ShoesId, Code, *Message, Id);
-                    FTimerHandle TH;
-                    GetWorldTimerManager().SetTimer(TH, [=]() {
-                        UE_LOG(LogTemp, Warning, TEXT("Try to remove %d"), Id);
-                        Avatar->RemoveBundle(Id);
-                        }, 10.0, false);
-                });
+	FTaskChain& Tasks = FTaskChain::Create(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+		INextHumanSDKModule::Get().PrepareResource(AvatarId, [=](int32 Code, const FString& Message) {
+			OnStepEnd(FTestRet{ Code, Message });
+		});
+	}).AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+		INextHumanSDKModule::Get().PrepareResource(BodyId, [=](int32 Code, const FString& Message) {
+			OnStepEnd(FTestRet{ Code, Message });
+		});
+	}).AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+		INextHumanSDKModule::Get().PrepareResource(CoverallId, [=](int32 Code, const FString& Message) {
+			OnStepEnd(FTestRet{ Code, Message });
+		});
+	}).AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+		INextHumanSDKModule::Get().PrepareResource(ClothId, [=](int32 Code, const FString& Message) {
+			OnStepEnd(FTestRet{ Code, Message });
+		});
+	}).AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+		INextHumanSDKModule::Get().PrepareResource(ShoesId, [=](int32 Code, const FString& Message) {
+			OnStepEnd(FTestRet{ Code, Message });
+		});
+	}).AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+		INextHumanSDKModule::Get().PrepareResource(TrousersId, [=](int32 Code, const FString& Message) {
+			OnStepEnd(FTestRet{ Code, Message });
+		});
+	}).AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+		Avatar->SetAvatarId(/* Put Avatar Id here */ AvatarId, [=](int32 Code, const FString& Message, int64 Id) {
+			OnStepEnd(FTestRet{ Code, Message });
+		});
+	});
 
-                Avatar->AddBundleById(TrouserId, [=](int32 Code, const FString& Message, int64 Id) {
-                    UE_LOG(LogTemp, Warning, TEXT("AddBundleById(%s): %d %s %d"), *TrouserId, Code, *Message, Id);
-                    FTimerHandle TH;
-                    GetWorldTimerManager().SetTimer(TH, [=]() {
-                        UE_LOG(LogTemp, Warning, TEXT("Try to remove %d"), Id);
-                        Avatar->RemoveBundle(Id);
-                        }, 15.0, false);
-                });
-            }
-        });
-    });
+	for (int i = 0; i < 1; i++) {
+		Tasks.AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+			Avatar->AddBundleById(ClothId, [=](int32 Code, const FString& Message, int64 Id) {
+				OnStepEnd(FTestRet{ Code, Message, Id });
+			});
+		}).AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+			Avatar->AddBundleById(ShoesId, [=](int32 Code, const FString& Message, int64 Id) {
+				OnStepEnd(FTestRet{ Code, Message, Last.ClothId, Id});
+			});
+		}).AndThen(ENamedThreads::AnyNormalThreadNormalTask, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+			Avatar->AddBundleById(TrousersId, [=](int32 Code, const FString& Message, int64 Id) {
+				OnStepEnd(FTestRet{ Code, Message, Last.ClothId, Last.ShoesId, Id });
+			});
+		}).AndThen(ENamedThreads::GameThread, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+			FTimerHandle TH;
+			GetWorldTimerManager().SetTimer(TH, [=]() {
+				Avatar->RemoveBundle(Last.ClothId);
+				OnStepEnd(FTestRet{ 0, TEXT(""), INT64_MIN, Last.ShoesId, Last.TrousersId });
+			}, 3.0, false);
+		}).AndThen(ENamedThreads::GameThread, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+			FTimerHandle TH;
+			GetWorldTimerManager().SetTimer(TH, [=]() {
+				Avatar->RemoveBundle(Last.ShoesId);
+				OnStepEnd(FTestRet{ 0, TEXT(""), INT64_MIN, INT64_MIN, Last.TrousersId });
+			}, 3.0, false);
+		}).AndThen(ENamedThreads::GameThread, [=](const FTestRet& Last, FTaskChain::FOnStepEnd OnStepEnd) {
+			FTimerHandle TH;
+			GetWorldTimerManager().SetTimer(TH, [=]() {
+				Avatar->RemoveBundle(Last.TrousersId);
+				OnStepEnd(FTestRet{ 0, TEXT(""), INT64_MIN, INT64_MIN, INT64_MIN });
+			}, 3.0, false);
+		});
+	}
+
+	Tasks.Start([=](const FTestRet& Last) {
+		UE_LOG(LogTemp, Warning, TEXT("==>> End"));
+	});
 }
---
+++ Source/SampleNHSDKUE427/Private/RunChain.h
@@ -0,0 +1,84 @@
+#pragma once
+
+namespace nexthuman
+{
+    namespace sdk
+    {
+        namespace demo {
+            struct FRet {
+                int32 Code;
+                FString Message;
+
+                FRet(int32 InCode, FString InMessage) : Code(InCode), Message(InMessage) {}
+            };
+
+            template <typename RET>
+            class TTaskChain {
+            public:
+                // TODO
+                //static_assert(std::is_base_of_v(FRet, RET), "RET must be FRet or subclass");
+                typedef TFunction<void(RET)> FOnStepEnd;
+                typedef TFunction<void(const RET& PreviousRet, FOnStepEnd)> FTask;
+                typedef TFunction<void(RET)> FCallback;
+
+                TTaskChain& AndThen(ENamedThreads::Type ThreadName, FTask Task) {
+                    Tasks.Add({ ThreadName, Task });
+                    return *this;
+                }
+
+                void Start(FCallback Callback) {
+                    if (Tasks.Num() == 0) {
+                        Callback(RET{ 0, TEXT("empty tasks") });
+                        return;
+                    }
+
+                    RunAt(0, RET{ 0, TEXT("") }, Callback);
+                }
+
+                static TTaskChain& Create(ENamedThreads::Type InThread, FTask FirstTask) {
+                    check(FirstTask);
+                    TTaskChain* Chain = new TTaskChain();
+                    return Chain->AndThen(InThread, FirstTask);
+                }
+
+                ~TTaskChain() {}
+                TTaskChain(const TTaskChain& Value) = delete;
+                TTaskChain(TTaskChain&& Value) = delete;
+                TTaskChain& operator=(const TTaskChain& Value) = delete;
+
+            private:
+                TTaskChain() {}
+
+                struct FTaskInfo
+                {
+                    ENamedThreads::Type ThreadName;
+                    FTask Task;
+                };
+
+                TArray<FTaskInfo> Tasks;
+                void RunAt(int32 Index, RET PreviousRet, FCallback Callback) {
+                    UE_LOG(LogTemp, Display, TEXT("RunAt Begin %d"), Index);
+                    check(Index >= 0 && Index < Tasks.Num());
+                    FTaskInfo& Task = Tasks[Index];
+                    AsyncTask(Task.ThreadName, [=]()
+                        {
+                            UE_LOG(LogTemp, Display, TEXT("Run %d"), Index);
+                            Task.Task(PreviousRet, [=](RET Ret)
+                                {
+                                    UE_LOG(LogTemp, Display, TEXT("RunAt End %d %d %s"), Index, Ret.Code, *Ret.Message);
+                                    if (Ret.Code != 0 || Index + 1 == Tasks.Num()) {
+                                        UE_LOG(LogTemp, Display, TEXT("Run Complete %d"), Index);
+                                        Callback(Ret);
+                                        return;
+                                    }
+
+                                    RunAt(Index + 1, Ret, Callback);
+                                });
+                        });
+                }
+            };
+
+            typedef TTaskChain<FRet> FTaskChain;
+        }
+    }
+}
