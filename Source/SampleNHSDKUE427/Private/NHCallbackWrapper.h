5f83a616b664c9d3a4ff7aa5482bfbd23260c3f6
Jiele Ye <yejiele1983@hotmail.com>
2023-04-27 15:03:53 +0800

    update demo according to new interfaces

---
+++ Source/SampleNHSDKUE427/Private/NHCallbackWrapper.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "NHPartInterface.h"
+#include "NHCallbackWrapper.generated.h"
+
+UCLASS()
+class UNHCallbackWrapper : public UObject {
+	GENERATED_UCLASS_BODY()
+public:
+
+	UFUNCTION(Category = "NextHuman")
+		void Run(int32 Code, const FString& Message, int64 Index);
+
+	void SetCallback(TFunction<void(int32, const FString&, int64)> InCallback);
+
+private:
+	TFunction<void(int32, const FString&, int64)> Callback;
+};
