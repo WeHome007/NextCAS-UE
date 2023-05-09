268e9383fb78f1e4fd89de1ffae973fc09640299
Jiele Ye <yejiele1983@hotmail.com>
2023-05-09 13:48:44 +0800

    update loader

--- Source/SampleNHSDKUE427/Public/AvatarLoader.h
+++ Source/SampleNHSDKUE427/Public/AvatarLoader.h
@@ -7,6 +7,7 @@
 #include "NHCallbackWrapper.h"
 #include "RunChain.h"
 #include "NextAvatar.h"
+#include "NHCategory.h"
 #include "AvatarLoader.generated.h"
 
 UCLASS()
@@ -34,6 +35,17 @@ private:
 		FTestRet(int32 InCode = 0, FString InMessage = TEXT(""), int64 InClothId = INT64_MIN, int64 InShoesId = INT64_MIN, int64 InTrousersId = INT64_MIN) :FRet(InCode, InMessage), ClothId(InClothId), ShoesId(InShoesId), TrousersId(InTrousersId) {}
 	};
 	typedef nexthuman::sdk::demo::TTaskChain<FTestRet> FTaskChain;
+	enum class EGender {
+		FEMALE,
+		MALE,
+	};
+
+	struct FAsset {
+		const FString Name;
+		const FString Id;
+		const EGender Gender;
+		const FString Category;
+	};
 
 	void LoadAvatar(const TMap<FString, FString>& Assets, const FVector& Position, const FRotator& Rotation);
 	void AddFaceBundle(FTaskChain& Chain, ANextAvatar* Avatar, const FString& Key, const FString& Id, TFunction<void(int64)> OnComplete = [](int64) {});
@@ -41,6 +53,10 @@ private:
 	void RemoveBodyBundle(FTaskChain& Chain, ANextAvatar* Avatar, const FString& Key);
 	void RemoveFaceBundle(FTaskChain& Chain, ANextAvatar* Avatar, const FString& Key);
 	void Delay(FTaskChain& Chain, float Seconds);
+
+	void Load(FString AvatarId, TArray<FAsset> Assets, const FVector& Position, const FRotator& Rotation);
+
 	TArray<UNHCallbackWrapper*> Callbacks;
 	TMap<FString, int64> IndexMap;
+	static const TArray<FAsset> ASSETS;
 };
