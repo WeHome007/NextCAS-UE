470291cc81a5777469aef806f67fe70f8c9e6b93
Jiele Ye <yejiele1983@hotmail.com>
2023-08-09 16:51:19 +0800

    add arg -nexthuman.sdk.host=

--- Source/Demo/Private/AvatarLoader.cpp
+++ Source/Demo/Private/AvatarLoader.cpp
@@ -26,29 +26,41 @@ void AAvatarLoader::BeginPlay()
 {
 	Super::BeginPlay();
 
-	static const FString Domain =
-		TEXT("");
-		//TEXT("https://open-meta.tmall.com");
+	FString Domain = TEXT("");
+	FString Host;
+	FParse::Value(FCommandLine::Get(), TEXT("nexthuman.sdk.host="), Host);
+	if (!Host.IsEmpty()) {
+		if (Host.Equals(TEXT("FC"), ESearchCase::IgnoreCase)) {
+			Domain = TEXT("https://open-meta.tmall.com");
+		}
+		else {
+			Domain = Host;
+		}
+	}
+	UE_LOG(LogTemp, Warning, TEXT("==>> Domain %s"), *Domain);
+		
 	if (!INextHumanSDKModule::Get().IsInitialized()) {
 		INextHumanSDKModule::Get().Initialize(
 			ACCESS_TOKEN,
 			[=](int32 Code, const FString& Message) {
 				if (Code == INextHumanSDKModule::CODE_SUCCESS && !FParse::Param(FCommandLine::Get(), TEXT("nexthuman.notest"))) {
 					if (Domain.IsEmpty()) {
-						auto Infos = FNHUtil::GetAvatarInfo();
-						for (int i = 0; i < Infos.Num(); i++) {
-							Load(Infos[i]);
-						}
+
+						//auto Infos = FNHUtil::GetAvatarInfo();
+						//for (int i = 0; i < Infos.Num(); i++) {
+						//	Load(Infos[i]);
+						//}
 
 						//Load(FEMALE_AVATAR_ID, {
-						//	TEXT("eyemakeup_64a5382a7868363aa03260bc"),
-						//	TEXT("blusher_64a538157868363aa03260bb"),
-						//	TEXT("eyelash_6482e3a7b325db3f950dca60"),
-						//	TEXT("eyebrow_645885a32a48eb54410d4c49"),
+						//	//TEXT("eyemakeup_64a5382a7868363aa03260bc"),
+						//	//TEXT("blusher_64a538157868363aa03260bb"),
+						//	TEXT("eyelash_6497f4ce7cee9a5058c1fa48"),
+						//	//TEXT("eyebrow_645885a32a48eb54410d4c49"),
 						//	});
 					}
 					else {
-						Load(TEXT("avatar_641884b0ba43910940e21054"));
+						//Load(TEXT("avatar_050118efed8f137bdcfee587dfb9fc2d"), {}, [](const FString&) { return true;  }, { 30, 0, 0});
+						//Load(TEXT("avatar_516090d479b98a97a21fd8a3fed10144"), {}, [](const FString&) { return true;  }, { -30, 0, 0 });
 					}
 				}
 			}, Domain);
